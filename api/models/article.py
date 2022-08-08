938be72088516ee76bb5e96dd29722636ee93ee7
kazuma <ikekazu120806@gmail.com>
2022-08-08 14:31:16 +0900

    マイグレーション成功

--- api/models/article.py
+++ api/models/article.py
@@ -12,11 +12,3 @@ class Article(Base):
 
     articleURL = Column(String(1024))
     # done = relationship("Done", back_populates="task")
-
-
-class Done(Base):
-    __tablename__ = "dones"
-
-    id = Column(Integer, ForeignKey("tasks.id"), primary_key=True)
-
-    task = relationship("Task", back_populates="done")
